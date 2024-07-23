3c0eaf1e947dce9d809217b3d05b1054fff5c55b
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-07-23 14:58:17 +0530

    [142] [Time Beats: 9.67%] [Memory Beats: 8.39%] - LeetPush

---
+++ contest/142-Linked-List-Cycle-II.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        Map<ListNode, Integer> map = new HashMap<>();
+        int index = 0;
+        while(head != null)
+        {
+            if(map.containsKey(head))
+            {
+                return head;
+            }
+            else
+            {
+                map.put(head, index++);
+            }
+
+            head = head.next;
+        }
+
+        return null;
+    }
+}
