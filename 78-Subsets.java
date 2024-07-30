b11cafc41ee50f7ff709a710d8e5be19ac8ac9a1
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-07-30 14:36:29 +0530

    [78] [Time Beats: 100.00%] [Memory Beats: 94.67%] - LeetPush

--- 78-Subsets.java
+++ 78-Subsets.java
@@ -1,21 +1,22 @@
 class Solution {
-    public List<List<Integer>> recursion(int index, int[] nums, List<Integer> temp, List<List<Integer>> original) {
-        original.add(new ArrayList<>(temp));
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
 
-        for (int i = index; i < nums.length; i++) {
-            if(!temp.contains(nums[i]))
-            temp.add(nums[i]);
-            recursion(i + 1, nums, temp, original);
-            temp.remove(temp.size() - 1);
+        createSubset(nums, 0, res, subset);
+        return res;        
+    }
+
+    private void createSubset(int[] nums, int index, List<List<Integer>> res, List<Integer> subset) {
+        if (index == nums.length) {
+            res.add(new ArrayList<>(subset));
+            return;
         }
 
-        return original;
-    }
+        subset.add(nums[index]);
+        createSubset(nums, index + 1, res, subset);
 
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> original = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        recursion(0, nums, temp, original);
-        return original;
-    }
+        subset.remove(subset.size() - 1);
+        createSubset(nums, index + 1, res, subset);
+    }    
 }
