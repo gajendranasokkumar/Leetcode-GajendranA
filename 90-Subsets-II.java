a619805369242128c5d4c51c713cde2bf8f5ea46
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-08-01 10:00:11 +0530

    [90] [Time Beats: 99.89%] [Memory Beats: 40.69%] - LeetPush

--- 90-Subsets-II.java
+++ 90-Subsets-II.java
@@ -1,26 +1,45 @@
 class Solution {
-    List<List<Integer>> result = new ArrayList<>();
+    // List<List<Integer>> result = new ArrayList<>();
 
-    public void fun(int[] nums, int n, int index, List<Integer> list) {
-        if (index == n) {
-            return;
-        }
-        list.add(nums[index]);
-        if (!result.contains(new ArrayList<>(list))) {
-            result.add(new ArrayList<>(list));
-            System.out.println(list);
-        }
+    // public void fun(int[] nums, int n, int index, List<Integer> list) {
+    //     if (index == n) {
+    //         return;
+    //     }
+    //     list.add(nums[index]);
+    //     if (!result.contains(new ArrayList<>(list))) {
+    //         result.add(new ArrayList<>(list));
+    //         System.out.println(list);
+    //     }
 
-        fun(nums, n, index + 1, list);
-        list.remove(list.size() - 1);
-        fun(nums, n, index + 1, list);
+    //     fun(nums, n, index + 1, list);
+    //     list.remove(list.size() - 1);
+    //     fun(nums, n, index + 1, list);
+    // }
+
+    // public List<List<Integer>> subsetsWithDup(int[] nums) {
+    //     Arrays.sort(nums);
+    //     List<Integer> li = new ArrayList<Integer>();
+    //     result.add(new ArrayList<>());
+    //     fun(nums, nums.length, 0, li);
+    //     return result;
+    // }
+
+
+    private void backtrack(int[] nums, int start, List<List<Integer>> ps, List<Integer> s) {
+        ps.add(new ArrayList<>(s));
+        int i = start;
+        while (i != nums.length) {
+            s.add(nums[i]);
+            backtrack(nums, i + 1, ps, s);
+            s.remove(s.size() - 1);
+            i++;
+            while (i < nums.length && nums[i] == nums[i - 1]) i++;
+        }
     }
-
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ps = new ArrayList<>();
         Arrays.sort(nums);
-        List<Integer> li = new ArrayList<Integer>();
-        result.add(new ArrayList<>());
-        fun(nums, nums.length, 0, li);
-        return result;
+        backtrack(nums, 0, ps, new ArrayList<>());
+        return ps;
     }
 }
