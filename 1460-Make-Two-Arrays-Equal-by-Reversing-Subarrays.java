499cb47ea5fb6d6b24c3756c66b88f60203074dd
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-08-03 11:45:26 +0530

    [1460] [Time Beats: 100.00%] [Memory Beats: 56.26%] - LeetPush

--- 1460-Make-Two-Arrays-Equal-by-Reversing-Subarrays.java
+++ 1460-Make-Two-Arrays-Equal-by-Reversing-Subarrays.java
@@ -1,29 +1,20 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        if(target.length != arr.length)
-            return false;
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<target.length;i++)
-        {
-            map.put(target[i], map.getOrDefault(target[i], 0) + 1);
+        int n = target.length, m = arr.length;
+
+        if (n != m) return false;
+
+        int[] map = new int[1001];
+
+        for (int i = 0; i < n; i++) {
+            map[target[i]]++;
+            map[arr[i]]--;
         }
 
-        System.out.println(map);
-
-        for(int i=0;i<arr.length;i++)
-        {
-            if(!map.containsKey(arr[i]))
+        for (int i = 0; i < n; i++) {
+            if (map[target[i]] != 0 || map[arr[i]] != 0) {
                 return false;
-            
-            map.put(arr[i] , map.get(arr[i]) - 1);
-        }
-
-        System.out.println(map);
-
-        for(Map.Entry<Integer, Integer> entry: map.entrySet())
-        {
-            if(entry.getValue() != 0)
-                return false;
+            }
         }
 
         return true;
