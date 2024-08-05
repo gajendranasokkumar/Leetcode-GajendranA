64c69e9c5bf27fee5bd1e272007df42d8bd42d0b
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-08-05 13:05:11 +0530

    [79] [Time Beats: 41.80%] [Memory Beats: 28.49%] - LeetPush

--- 79-Word-Search.java
+++ 79-Word-Search.java
@@ -6,7 +6,6 @@ class Solution {
 
         int index = 0;
 
-        // First search the first character
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
 
@@ -22,29 +21,21 @@ class Solution {
 
     private boolean searchNext(char[][] board, String word, int row, int col, int index, int m, int n) {
 
-        // if index reaches at the end that means we have found the word
         if (index == word.length())
             return true;
 
-        // Checking the boundaries if the character at which we are placed is not 
-        //the required character
         if (row < 0 || col < 0 || row == m || col == n || board[row][col] != word.charAt(index) || board[row][col] == '!')
             return false;
 
-        // this is to prevent reusing of the same character
         char c = board[row][col];
         board[row][col] = '!';
 
-        // top direction
         boolean top = searchNext(board, word, row - 1, col, index + 1, m, n);
-        // right direction
         boolean right = searchNext(board, word, row, col + 1, index + 1, m, n);
-        // bottom direction
         boolean bottom = searchNext(board, word, row + 1, col, index + 1, m, n);
-        // left direction
         boolean left = searchNext(board, word, row, col - 1, index + 1, m, n);
 
-        board[row][col] = c; // undo change
+        board[row][col] = c; 
 
         return top || right || bottom || left;
     }
