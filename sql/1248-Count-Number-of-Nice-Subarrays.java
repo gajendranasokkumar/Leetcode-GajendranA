8c5be99aa3d3e0541300ec4774d82bc6ef4c9a8a
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-09-02 15:20:17 +0530

    [1248] [Time Beats: 92.42%] [Memory Beats: 96.06%] - LeetPush

--- sql/1248-Count-Number-of-Nice-Subarrays.java
+++ sql/1248-Count-Number-of-Nice-Subarrays.java
@@ -1,24 +1,21 @@
-// class Solution {
-//     public int numberOfSubarrays(int[] nums, int k) {
-        
-//     }
-// }
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        Map<Integer, Integer> prefixSumCount = new HashMap<>();
-        prefixSumCount.put(0, 1);  
+        int n = nums.length;
+        int[] prefixSumCount = new int[n + 1]; 
+        prefixSumCount[0] = 1;
 
         int sum = 0, count = 0;
         
         for (int num : nums) {
-            if(num%2!=0)
+            if (num % 2 != 0) {
                 sum++;
-            
-            if (prefixSumCount.containsKey(sum - k)) {
-                count += prefixSumCount.get(sum - k);
             }
             
-            prefixSumCount.put(sum, prefixSumCount.getOrDefault(sum, 0) + 1);
+            if (sum >= k) {
+                count += prefixSumCount[sum - k]; 
+            }
+            
+            prefixSumCount[sum]++;
         }
         
         return count;
