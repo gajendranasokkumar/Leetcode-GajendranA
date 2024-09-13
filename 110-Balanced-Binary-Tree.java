5ea750676aa8add73ca9641d64fe204198e301ab
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-09-13 09:34:34 +0530

    [110] [Time Beats: 100.00%] [Memory Beats: 86.55%] - LeetPush

--- 110-Balanced-Binary-Tree.java
+++ 110-Balanced-Binary-Tree.java
@@ -1,43 +1,37 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null)
+            return true;
+        if (h(root) == -1)
+            return false;
 
-    public int height(TreeNode temp)
-    {
-        if(temp == null)
-            return -1;
-        else
-        {
-            int left = height(temp.left);
-            int right = height(temp.right);
-            if(left > right)
-                return left+1;
-            else 
-                return right+1;
-        }
+        return true;
     }
 
-    public boolean isBalanced(TreeNode root) {
-        if(root == null) return true;
-        int left = height(root.left)+1;
-        int right = height(root.right)+1;
-        System.out.println(left + \ \ + right);
-        if (Math.abs(left - right) > 1) {
-            return false;
-        }
-        return isBalanced(root.left) && isBalanced(root.right);
+    static int h(TreeNode root) {
+        if (root == null)
+            return 0;
+        int lh = h(root.left);
+        int rh = h(root.right);
+
+        if (lh == -1 || rh == -1 || (Math.abs(lh - rh) > 1))
+            return -1;
+
+        return Math.max(lh, rh) + 1;
     }
 }
