7678303f18c8907cf81a00505968f9cd387fa502
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-09-25 13:40:44 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 77.49%] - LeetPush

---
+++ 198-House-Robber.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int rob(int[] nums) {
+        int prev1 = 0, prev2 = 0;
+        for (int num : nums) {
+            int temp = prev1;
+            prev1 = Math.max(prev2 + num, prev1);
+            prev2 = temp;
+        }
+        return prev1;
+    }
+}
