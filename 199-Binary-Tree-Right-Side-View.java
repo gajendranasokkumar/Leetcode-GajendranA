9efcc68c0a0313b6c5392b646268b959b28a20dc
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-10-05 10:33:20 +0530

    [199] [Time Beats: 65.30%] [Memory Beats: 45.97%] - LeetPush

--- 199-Binary-Tree-Right-Side-View.java
+++ 199-Binary-Tree-Right-Side-View.java
@@ -13,35 +13,28 @@
  *     }
  * }
  */
-
-class NodeLevel {
-    int level;
-    TreeNode node;
-    public NodeLevel(int l, TreeNode n) {
-        this.level = l;
-        this.node = n;
-    }
-}
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        if(root == null)
+        if (root == null) 
             return new ArrayList<>();
+
         List<Integer> list = new ArrayList<>();
-        Queue<TreeNode> queue1 = new LinkedList<>();
-        Queue<TreeNode> queue2 = new LinkedList<>();
-        queue1.add(root);
-        while(!queue1.isEmpty()) {
-            while(queue1.size() > 1)
-                queue2.add(queue1.poll());
-            TreeNode temp = queue1.poll();
-            list.add(temp.val);
-            queue2.add(temp);
-            while(!queue2.isEmpty()) {
-                TreeNode cur = queue2.poll();
-                if(cur.left != null)
-                    queue1.add(cur.left);
-                if(cur.right != null)
-                    queue1.add(cur.right);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size(); 
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                
+                if (i == size - 1) {
+                    list.add(node.val);
+                }
+                
+                if (node.left != null) 
+                    queue.add(node.left);
+                if (node.right != null) 
+                    queue.add(node.right);
             }
         }
 
