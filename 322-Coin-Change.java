92f72f0a585a3ed389f965a5351a022bf10a3de6
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-10-06 21:26:33 +0530

    [322] [Time Beats: 14.25%] [Memory Beats: 33.63%] - LeetPush

--- 322-Coin-Change.java
+++ 322-Coin-Change.java
@@ -1,21 +1,28 @@
-import java.util.Arrays;
-
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        
-        Arrays.fill(dp, amount + 1);
-        
-        dp[0] = 0;
-        
-        for (int a = 1; a <= amount; a++) {
-            for (int coin : coins) {
-                if (coin <= a) {
-                    dp[a] = Math.min(dp[a], 1 + dp[a - coin]);
-                }
-            }
+     public static int minimum(int i,int target,int arr[],int dp[][]){
+        if(i==0){
+            if(target%arr[i]==0)
+                return target/arr[i];
+            else
+                return (int)Math.pow(10,9);
         }
-        
-        return dp[amount] > amount ? -1 : dp[amount];
+        if(target==0)
+            return 0;
+        if(dp[i][target]!=-1)
+            return dp[i][target];
+        int take = Integer.MAX_VALUE;
+        int nottake =0+ minimum(i-1,target,arr,dp);
+        if(arr[i]<=target)
+            take = 1+ minimum(i,target-arr[i],arr,dp);
+        return dp[i][target] = Math.min(take,nottake);        
+    }
+    public static int coinChange(int num[], int x) {
+        int [][] dp = new int[num.length][x+1];
+        for(int i[] : dp)
+            Arrays.fill(i,-1);
+        int res = minimum(num.length-1,x,num,dp);
+        if (res >= (int) Math.pow(10, 9))
+            return -1;
+        return res;
     }
 }
