33ac3438a5555b768274dc3497a9c9bfc61e65c9
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-10-07 06:35:07 +0530

    [2696] [Time Beats: 36.18%] [Memory Beats: 27.11%] - LeetPush

--- 2696-Minimum-String-Length-After-Removing-Substrings.java
+++ 2696-Minimum-String-Length-After-Removing-Substrings.java
@@ -1,26 +1,9 @@
 class Solution {
     public int minLength(String s) {
-        Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char cur_char = s.charAt(i);
-
-            if (stack.isEmpty()) {
-                stack.push(cur_char);
-                continue;
-            }
-      
-            if (cur_char == 'B' && stack.peek() == 'A') {
-                stack.pop();
-            }
-            else if (cur_char == 'D' && stack.peek() == 'C') {
-                stack.pop();
-            }
-            else {
-                stack.push(cur_char);
-            }
+        while(s.contains("AB") || s.contains("CD")) {
+            s = s.replace("AB", "");
+            s = s.replace("CD", "");
         }
-
-        return stack.size();
+        return s.length();
     }
 }
