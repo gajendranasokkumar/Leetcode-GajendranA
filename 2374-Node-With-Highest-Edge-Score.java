c4cff2f9fa404f772209b4a5f03657ed015c95e6
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-10-07 16:07:07 +0530

    [2374] [Time Beats: 6.79%] [Memory Beats: 5.43%] - LeetPush

---
+++ 2374-Node-With-Highest-Edge-Score.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int edgeScore(int[] edges) {
+        Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+        for(int i=0;i<edges.length;i++) {
+            map.putIfAbsent(edges[i], new ArrayList<Integer>());
+            map.get(edges[i]).add(i);
+        }
+        System.out.println(map);
+        long max = -1; int node = 0;
+        for(int i=0;i<edges.length;i++) {
+            ArrayList<Integer> list = map.getOrDefault(i, new ArrayList<>());
+            long sum = 0;
+            for(int j=0;j<list.size();j++) {
+                sum += list.get(j);
+            }
+            if(max < sum) {
+                max = sum;
+                node = i;
+            }
+        }
+        return node;
+    }
+}
