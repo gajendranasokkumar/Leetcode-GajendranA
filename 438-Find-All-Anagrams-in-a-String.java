e1d7360f53809bf6404d6bf18674008efaff81a3
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-10-08 06:05:23 +0530

    [438] [Time Beats: 48.28%] [Memory Beats: 91.62%] - LeetPush

--- 438-Find-All-Anagrams-in-a-String.java
+++ 438-Find-All-Anagrams-in-a-String.java
@@ -1,27 +1,44 @@
 class Solution {
-    public boolean isAnagram(char[] s, char t[]) {
-        int [] arr = new int[26];
-        for(char ch: s)
-            arr[ch - 97] += 1;
-        for (char ch: t)
-            arr[ch - 97] -= 1;
-        for (int count: arr)
-            if (count != 0) return false;
-        return true;
-    }
     public List<Integer> findAnagrams(String s, String p) {
-        int finish = p.length();
-        List<Integer> indexList= new ArrayList<>();
-        if (s.length() < p.length())
-            return indexList;
-        char[] pStr = p.toCharArray();
-        char[] sStr = s.toCharArray();
-        while (finish <= s.length())
-        {
-            if (isAnagram(Arrays.copyOfRange(sStr,finish - p.length(),finish), pStr))
-                indexList.add(finish - p.length());
-            finish++;
+        List<Integer> result = new LinkedList<>();
+        if(p.length() > s.length()) return result;
+
+        Map<Character, Integer> charCountMap = new HashMap<>();
+        for(char c : p.toCharArray()){
+            charCountMap.put(c, charCountMap.getOrDefault(c,0) + 1);
         }
-        return indexList;
+
+        int matchCounter = charCountMap.size();
+
+        int left = 0, right = 0;
+
+        while(right < s.length()){
+            char rightChar = s.charAt(right);
+
+            if(charCountMap.containsKey(rightChar)){
+                charCountMap.put(rightChar, charCountMap.get(rightChar) - 1);
+                if(charCountMap.get(rightChar) == 0){
+                    matchCounter--;
+                }
+            }
+            right++;
+
+            while(matchCounter == 0){
+                char leftChar = s.charAt(left);
+
+                if(right-left == p.length()){
+                    result.add(left);
+                }
+
+                if(charCountMap.containsKey(leftChar)){
+                    charCountMap.put(leftChar, charCountMap.get(leftChar) + 1);
+                    if(charCountMap.get(leftChar) >0){
+                        matchCounter++;
+                    }
+                }
+                left++;
+            }
+        }
+        return result;
     }
 }
