b8c400ff1febec0fb11d6da0ec77b6227f20425e
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-10-10 12:27:54 +0530

    [200] [Time Beats: 100.00%] [Memory Beats: 52.60%] - LeetPush

--- 200-Number-of-Islands.java
+++ 200-Number-of-Islands.java
@@ -1,31 +1,34 @@
 class Solution {
+    int rows;
+    int cols;
+    int islands = 0;
+    
     public int numIslands(char[][] grid) {
-        int count = 0;
-        int rows = grid.length;
-        int cols = grid[0].length;
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == '1') { 
-                    count++;  
-                    dfs(grid, i, j);  
-                }
-            }
-        }
-
-        return count;
+        rows = grid.length;
+        cols = grid[0].length;
+        for (int row = 0; row < rows; row++)
+            check(grid, row);
+        return islands;
     }
 
-    private void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
-            return;
-        }
-
-        grid[i][j] = '0';
-
-        dfs(grid, i - 1, j);
-        dfs(grid, i + 1, j); 
-        dfs(grid, i, j - 1);
-        dfs(grid, i, j + 1); 
+    public void check(final char[][] grid, int row) {
+        final char[] finalRow = grid[row];
+        for (int col = 0; col < cols; ++col)
+            if (finalRow[col] == '1') {
+                bfs(grid, row, col);
+                ++islands;
+            }
+    }
+    
+    public void bfs(char[][] grid, int row, int col) {
+        grid[row][col] = '*';
+        if (row > 0 && grid[row - 1][col] == '1')
+            bfs(grid, row - 1, col);
+        if (row + 1 < rows && grid[row + 1][col] == '1')
+            bfs(grid, row + 1, col);
+        if (col > 0 && grid[row][col - 1] == '1')
+            bfs(grid, row, col - 1);
+        if (col + 1 < cols && grid[row][col + 1] == '1')
+            bfs(grid, row, col + 1);
     }
 }
