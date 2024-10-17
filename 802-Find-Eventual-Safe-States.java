2b05b81cac9acf98e2ea5391ff7722d220309a01
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-10-17 21:51:42 +0530

    [802] [Time Beats: 9.76%] [Memory Beats: 9.27%] - LeetPush

--- 802-Find-Eventual-Safe-States.java
+++ 802-Find-Eventual-Safe-States.java
@@ -1,40 +1,43 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n = graph.length;
-        List<List<Integer>> lis = new ArrayList<List<Integer>>();
-        for (int i = 0; i < graph.length; i++) {
-            List<Integer> p = new ArrayList<Integer>();
-            for (int j = 0; j < graph[i].length; j++) {
-                p.add(graph[i][j]);
+        Map<Integer, ArrayList<Integer>> adj = new HashMap<>();
+        int len = graph.length;
+        for(int i=0;i<len;i++) {
+            for(int j=0;j<graph[i].length;j++) {
+                adj.putIfAbsent(graph[i][j], new ArrayList<Integer>());
+                adj.get(graph[i][j]).add(i);
             }
-            lis.add(p);
         }
-        List<Integer> result = new ArrayList<Integer>();
-        for (int i = 0; i < n; i++) {
-            boolean visited[] = new boolean[n];
-            boolean pathvisited[] = new boolean[n];
-            if (!backtrack(i, visited, pathvisited, lis)) {
-                result.add(i);
-            }
+
+        Queue<Integer> q = new LinkedList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+
+        int[] indegree = new int[len];
+        for(int i=0;i<len;i++) {
+            if(adj.containsKey(i))
+                for(int j: adj.get(i))
+                    indegree[j]++;
+        }
+
+        for(int i=0;i<len;i++) 
+            if(indegree[i] == 0)
+                q.add(i);
+
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            result.add(node);
+            if(adj.containsKey(node))
+                for (int neighbor : adj.get(node)) {
+                    indegree[neighbor]--;
+                    if (indegree[neighbor] == 0) {
+                        q.offer(neighbor);
+                    }
+                }
         }
 
         Collections.sort(result);
+        
         return result;
-    }
-
-    boolean backtrack(int node, boolean[] visited, boolean[] pathvisited, List<List<Integer>> lis) {
-        visited[node] = true;
-        pathvisited[node] = true;
-        List<Integer> l = lis.get(node);
-        for (int i : l) {
-            if (!visited[i]) {
-                if (backtrack(i, visited, pathvisited, lis)) {
-                    return true;
-                }
-            } else if (pathvisited[i])
-                return true;
-        }
-        pathvisited[node] = false;
-        return false;
     }
 }
