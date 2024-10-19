44e77c994214ade17b29952c2687761b28a7beb7
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-10-19 06:52:06 +0530

    [1545] [Time Beats: 100.00%] [Memory Beats: 85.41%] - LeetPush

--- 1545-Find-Kth-Bit-in-Nth-Binary-String.java
+++ 1545-Find-Kth-Bit-in-Nth-Binary-String.java
@@ -1,24 +1,25 @@
 class Solution {
     public char findKthBit(int n, int k) {
-        
-        String[] arr = new String[21];
-        arr[1] = "0";
-        for(int i=2;i<21;i++) {
-            arr[i] = arr[i-1] + "1" + reverse(invert(arr[i-1]));
+        if (n == 1) {
+            return '0';
         }
-
-        return arr[n].charAt(k-1);
-    }
-
-    public String invert(String original) {
-        StringBuilder newstr = new StringBuilder();
-        for(int i=0;i<original.length();i++)
-            newstr.append((original.charAt(i) == '1') ? "0" : "1");
-        return newstr.toString();
-    }
-
-    public String reverse(String original) {
-        StringBuilder newstr = new StringBuilder(original);
-        return newstr.reverse().toString();
+        
+        int length = (1 << n) - 1;  // 2^n - 1
+        
+        // The middle bit in S_n is at position (length / 2) + 1
+        int mid = length / 2 + 1;
+        
+        if (k == mid) {
+            // If k is the middle bit, it is always '1'
+            return '1';
+        } else if (k < mid) {
+            // If k is in the first half, it is the same as in S_{n-1}
+            return findKthBit(n - 1, k);
+        } else {
+            // If k is in the second half, find the corresponding bit in the reverse-inverted S_{n-1}
+            // The corresponding bit in the first half of S_{n-1} is at position (length - k + 1)
+            char invertedBit = findKthBit(n - 1, length - k + 1);
+            return (invertedBit == '0') ? '1' : '0';
+        }
     }
 }
