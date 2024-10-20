724d18f76b35dafd06d15f533a1439bfe00eda40
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-10-20 14:04:33 +0530

    [1106] [Time Beats: 69.27%] [Memory Beats: 68.77%] - LeetPush

--- 1106-Parsing-A-Boolean-Expression.java
+++ 1106-Parsing-A-Boolean-Expression.java
@@ -2,46 +2,32 @@ class Solution {
     public boolean parseBoolExpr(String expression) {
         Stack<Character> stack = new Stack<>();
 
-        for(char i : expression.toCharArray()) {
-            if(i != ',')
-                stack.push(i);            
+        for (char ch : expression.toCharArray()) {
 
-            char top = stack.peek();
-            if(top == ')') {
+            if (ch == ')') {
                 int tr = 0, fl = 0;
-                stack.pop();
-                while(stack.peek() != '(') {
-                    char one = stack.pop();
-                    if(one == 't')
+                while (stack.peek() != '(') {
+                    char val = stack.pop();
+                    if (val == 't') 
                         tr++;
-                    else 
+                    else if (val == 'f') 
                         fl++;
                 }
                 stack.pop();
-                char oper = stack.pop();
-
-                if(oper == '&') {
-                    if(fl != 0)
-                        stack.push('f');
-                    else
-                        stack.push('t');
-                } 
-                else if(oper == '|') {
-                    if(tr == 0)
-                        stack.push('f');
-                    else
-                        stack.push('t');
-                }
-                else if(oper == '!') {
-                    if(tr == 0 && fl != 0)
-                        stack.push('t');
-                    else
-                        stack.push('f');
-                }
+                char operator = stack.pop();
+                
+                if (operator == '&') 
+                    stack.push(fl > 0 ? 'f' : 't');
+                else if (operator == '|') 
+                    stack.push(tr > 0 ? 't' : 'f');
+                else if (operator == '!') 
+                    stack.push(fl > 0 ? 't' : 'f');
+            
+            } else if (ch != ',') {
+                stack.push(ch);
             }
-
         }
 
-        return stack.peek() == 't' ? true : false;
+        return stack.pop() == 't';
     }
 }
