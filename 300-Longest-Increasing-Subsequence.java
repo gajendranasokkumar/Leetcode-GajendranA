c96e97794bb672595780721e385f5f7d56f883d3
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-11-04 12:01:44 +0530

    [300] [Time Beats: 20.71%] [Memory Beats: 46.44%] - LeetPush

--- 300-Longest-Increasing-Subsequence.java
+++ 300-Longest-Increasing-Subsequence.java
@@ -1,24 +1,31 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
+    public int lengthOfLIS(int[] arr) {
+        int n = arr.length;
+        int res = 1;
+        int[] memo = new int[n];
+        Arrays.fill(memo, -1);
+        
+        for (int idx = 1; idx < n; idx++)
+            res = Math.max(res, lisEndingAtIdx(arr, idx, memo));
+        
+        return res;
+    }
 
-        for (int i = 0; i < n; i++) {
-            for (int k = i-1; k >= 0; k--) {
-                if (nums[k] < nums[i]) {
-                    dp[i] = Math.max(dp[i], dp[k]);
-                }
+    static int lisEndingAtIdx(int[] arr, int idx, int[] memo) {      
+        if (memo[idx] != -1) 
+            return memo[idx];
+        
+        if (idx == 0)
+            return 1;
+
+        int mx = 1;
+        for (int prev = 0; prev < idx; prev++) {
+            if (arr[prev] < arr[idx]) {
+                mx = Math.max(mx, lisEndingAtIdx(arr, prev, memo) + 1);
             }
-            dp[i]+=1;
         }
-
-        int highest = 1;
-        for (int len : dp) {
-            if (len > highest) {
-                highest = len;
-            }
-        }
-
-        return highest;
+        
+        memo[idx] = mx;
+        return mx;
     }
 }
