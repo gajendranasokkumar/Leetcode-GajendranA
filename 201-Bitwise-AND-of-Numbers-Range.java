9b039736a4b7e1c13e73fd83a1114cc655f47c64
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-11-05 11:06:51 +0530

    [201] [Time Beats: 100.00%] [Memory Beats: 35.95%] - LeetPush

--- 201-Bitwise-AND-of-Numbers-Range.java
+++ 201-Bitwise-AND-of-Numbers-Range.java
@@ -1,17 +1,8 @@
 class Solution {
     public int rangeBitwiseAnd(int left, int right) {
-        int ans = 0;
-        for(int i = 30; i >= 0; i--) {
-            int bit = 1 << i;
-            // Check if both numbers have the same bit set
-            if((left & bit) == (right & bit)) {
-                // If both have the same bit, add it to answer
-                ans |= (left & bit);
-            } else {
-                // If bits are different, break as all remaining bits will be 0
-                break;
-            }
+        while (right > left) {
+            right = right & (right - 1);
         }
-        return ans;
+        return right;
     }
 }
