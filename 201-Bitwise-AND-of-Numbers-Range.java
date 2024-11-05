97c53f5d66fa1b185ce8a6f0ffe1322609d1c055
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-11-05 11:27:44 +0530

    [201] [Time Beats: 10.83%] [Memory Beats: 98.47%] - LeetPush

--- 201-Bitwise-AND-of-Numbers-Range.java
+++ 201-Bitwise-AND-of-Numbers-Range.java
@@ -1,8 +1,21 @@
 class Solution {
     public int rangeBitwiseAnd(int left, int right) {
-        while (right > left) {
-            right = right & (right - 1);
+        int ans=0;
+
+        for(int i=30;i>=0;i--){
+            long holdle = (long)(Math.pow(2,i));
+            
+            long hold=(long)(Math.pow(2,i+1))-1;
+            
+
+            if(holdle <=left && hold>=right){
+                ans=  ans^(1<<i);
+                left-=holdle;
+                right-=holdle;
+            
+            }
         }
-        return right;
+
+        return ans;
     }
 }
