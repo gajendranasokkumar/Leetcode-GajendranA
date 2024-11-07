83b56964db0211c607c3639cf60fd9741a7c4b93
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-11-07 08:17:48 +0530

    [2275] [Time Beats: 53.97%] [Memory Beats: 43.03%] - LeetPush

--- 2275-Largest-Combination-With-Bitwise-AND-Greater-Than-Zero.java
+++ 2275-Largest-Combination-With-Bitwise-AND-Greater-Than-Zero.java
@@ -6,8 +6,9 @@ class Solution {
         int max = 0;
         for(int i=0;i<32;i++) { 
             int count=0;
+            int b = (1 << i);
             for(int num : nums) {
-                if((num & (1 << i)) != 0)
+                if((num & b) != 0)
                     count++;
             }
             max = Math.max(max, count);
