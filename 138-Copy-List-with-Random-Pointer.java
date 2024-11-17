8a2f29bd3df0f9cd80690ed01c1567a6cef207c7
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-11-17 14:08:52 +0530

    [138] [Time Beats: 100.00%] [Memory Beats: 43.35%] - LeetPush

--- 138-Copy-List-with-Random-Pointer.java
+++ 138-Copy-List-with-Random-Pointer.java
@@ -1,58 +1,22 @@
-/*
-// Definition for a Node.
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
-*/
-
-class Solution {
+public class Solution {
     public Node copyRandomList(Node head) {
-        Node temp = head;
-        Node newlist = new Node(0), t=newlist;
-        while(temp != null)
-        {
-            t.next = new Node(temp.val);
-            t=t.next;
-            temp=temp.next;
+        if (head == null) return null;
+        
+        HashMap<Node, Node> oldToNew = new HashMap<>();
+        
+        Node curr = head;
+        while (curr != null) {
+            oldToNew.put(curr, new Node(curr.val));
+            curr = curr.next;
         }
-        temp = head;
-        t = newlist.next;
-        while(temp != null)
-        {
-            int count = 0;
-            Node search = temp.random, s = head;
-            if(search != null)
-            {
-                while(s != null && s != search)
-                {
-                    count++;
-                    s = s.next;
-                }
-                Node point = newlist;
-                while(count >= 0)
-                {
-                    point = point.next;
-                    count--;
-                }
-
-                t.random = point;
-            }
-            else
-            {
-                t.random = null;
-            }
-
-            temp = temp.next;
-            t = t.next;
+        
+        curr = head;
+        while (curr != null) {
+            oldToNew.get(curr).next = oldToNew.get(curr.next);
+            oldToNew.get(curr).random = oldToNew.get(curr.random);
+            curr = curr.next;
         }
-        return newlist.next;
+        
+        return oldToNew.get(head);
     }
 }
