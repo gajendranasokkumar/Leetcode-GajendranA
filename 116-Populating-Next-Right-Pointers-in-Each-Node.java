13e80e1207eb6b7f11ffd0c919d805f154ecda36
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-11-17 11:48:38 +0530

    [116] [Time Beats: 100.00%] [Memory Beats: 6.35%] - LeetPush

--- 116-Populating-Next-Right-Pointers-in-Each-Node.java
+++ 116-Populating-Next-Right-Pointers-in-Each-Node.java
@@ -23,32 +23,21 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        if (root == null) {
+        if(root==null)
+        {
             return null;
         }
-
-        // Start with the root node
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(root);
-        
-        while (!queue.isEmpty()) {
-            int levelSize = queue.size();  // Number of nodes at the current level
-            
-            // Traverse nodes of the current level
-            for (int i = 0; i < levelSize; i++) {
-                Node node = queue.poll();
-                
-                // Connect the node to the next node in the queue (if not the last node)
-                if (i < levelSize - 1) {
-                    node.next = queue.peek();
-                }
-                
-                // Add the child nodes to the queue for the next level
-                if (node.left != null) queue.offer(node.left);
-                if (node.right != null) queue.offer(node.right);
-            }
+        if(root.left!=null)
+        {
+            root.left.next=root.right;
         }
+        if(root.right!=null&&root.next!=null)
+        {
+            root.right.next=root.next.left;
+        }
+        connect(root.left);
+        connect(root.right);
+        return root;
         
-        return root;  // Return the root node, now with next pointers connected
     }
 }
