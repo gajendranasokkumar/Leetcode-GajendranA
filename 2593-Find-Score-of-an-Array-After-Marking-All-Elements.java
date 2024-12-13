b6c2198c7ebe86f5f25d00f35a2c2a9365faa56a
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-12-13 12:31:42 +0530

    [2593] [Time Beats: 98.53%] [Memory Beats: 58.33%] - LeetPush

--- 2593-Find-Score-of-an-Array-After-Marking-All-Elements.java
+++ 2593-Find-Score-of-an-Array-After-Marking-All-Elements.java
@@ -1,65 +1,54 @@
 class Solution {
     public long findScore(int[] nums) {
-        long sum = 0;
-        int prev = -1;
-        if(nums.length == 1)
+        if (nums.length == 1) {
             return nums[0];
-            
-        for(int i=0;i<nums.length;i++) {
-            if(prev == -1) {
-                if(nums[i] <= nums[i+1]) {
-                    sum += nums[i];
-                    nums[i] = -1;
-                    nums[i+1] = -1;
-                }
-            }
-            else if(prev == nums.length-2) {
-                if(nums[prev] != -1 && nums[i] != -1 && nums[prev] >= nums[i]) {
-                    sum += nums[i];
-                    nums[prev] = -1;
-                    nums[i] = -1;
-                }
-            }
-            else if(nums[i] != -1 && ((nums[prev] >= nums[i] && nums[i] <= nums[i+1]) || (nums[prev] == -1 && nums[i] <= nums[i+1]))) {
-                sum += nums[i];
-                nums[prev] = -1;
-                nums[i] = -1;
-                nums[i+1] = -1;
-            }
-            prev = i;
         }
-    
-        prev = -1;
+
+        long s = 0;
+        int p = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != Integer.MAX_VALUE) {
+                if (i < nums.length - 1 && nums[i] <= p && nums[i] <= nums[i + 1]) {
+                    s = s + nums[i];
+                    nums[i + 1] = Integer.MAX_VALUE;
+                    if (i - 1 >= 0) {
+                        nums[i - 1] = Integer.MAX_VALUE;
+                    }
+                    nums[i] = Integer.MAX_VALUE;
+                }
+                else if (i == nums.length - 1) {
+
+                    if (nums[i] <= p) {
+                        s = s + nums[i];
+
+                        nums[i - 1] = Integer.MAX_VALUE;
+                        nums[i] = Integer.MAX_VALUE;
+                    }
+                }
+            }
+            p = nums[i];
+        }
+
+        p = Integer.MAX_VALUE;
         for (int i = nums.length - 1; i >= 0; i--) {
-            if (prev == -1) {
-                if (nums[i] != -1 && nums[i] <= nums[i-1]) { 
-                    sum += nums[i];
-                    nums[i] = -1;
-                    nums[i - 1] = -1;
+            if (nums[i] != Integer.MAX_VALUE) {
+                if (i > 0 && nums[i] <= p && nums[i] <= nums[i - 1]) {
+                    s = s + nums[i];
+                    nums[i - 1] = Integer.MAX_VALUE;
+                    if (i + 1 < nums.length) {
+                        nums[i + 1] = Integer.MAX_VALUE;
+                    }
+                    nums[i] = Integer.MAX_VALUE;
+                } else if (i == 0) {
+                    if (nums[i] <= p) {
+                        s = s + nums[i];
+                        nums[i] = Integer.MAX_VALUE;
+                    }
                 }
             }
-            else if (prev == 1) {
-                if (nums[i] != -1 && nums[prev] != -1 && nums[prev] >= nums[i]) {
-                    sum += nums[i];
-                    nums[prev] = -1;
-                    nums[i] = -1;
-                }
-            }
-            else if (nums[i] != -1 && nums[prev] == -1 && nums[i] <= nums[i - 1]) {
-                sum += nums[i];
-                nums[i] = -1;
-                nums[i - 1] = -1;
-            }
-            prev = i;
+            p = nums[i];
         }
 
-        for(int i: nums) 
-            if(i != -1)
-                sum += i;
-        
-
-        System.out.println(Arrays.toString(nums));
-        
-        return sum;
+        return s;
     }
 }
