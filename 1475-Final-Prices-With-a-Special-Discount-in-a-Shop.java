1fe61804df73c70e83a83459c90408b0605801e0
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-12-18 07:35:10 +0530

    [1475] [Time Beats: 98.90%] [Memory Beats: 31.07%] - LeetPush

--- 1475-Final-Prices-With-a-Special-Discount-in-a-Shop.java
+++ 1475-Final-Prices-With-a-Special-Discount-in-a-Shop.java
@@ -1,21 +1,18 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
         int n = prices.length;
-        int[] result = new int[n];
-        Stack<Integer> stack = new Stack<>();
-        
-        for (int i = 0; i < n; i++) {
-            result[i] = prices[i];
-        }
-
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i]) {
-                int index = stack.pop();
-                result[index] -= prices[i];
+        if(n<=1)
+            return prices;
+        int[] ans = new int[n];
+        for(int i=0; i<n; i++){
+            ans[i] = prices[i];
+            for(int j=i+1; j<n; j++){
+                if(prices[j]<=prices[i]){
+                    ans[i] = prices[i] - prices[j];
+                    break;
+                }
             }
-            stack.push(i);
         }
-        
-        return result;
+        return ans;
     }
 }
