a28c5e913295250a443388681e97ec62283a404d
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-12-25 15:32:33 +0530

    Time: 0 ms (100%), Space: 42.3 MB (8.42%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            // Extract the last bit of n and add it to the result
+            result = (result << 1) | (n & 1);
+            // Shift n to the right to process the next bit
+            n >>= 1;
+        }
+        return result;
+    }
+}
