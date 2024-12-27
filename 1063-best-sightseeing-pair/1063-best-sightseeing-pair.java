c2d27b66efe7ae7e6bfe8f2e5233d11dc37d4a0c
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2024-12-27 10:22:43 +0530

    Time: 16 ms (5.13%), Space: 59.8 MB (5.14%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
@@ -23,3 +23,17 @@ class Solution {
         return dp[count][idx] = Math.max(pick, notpick);
     }
 }
+
+
+// class Solution {
+//     public int maxScoreSightseeingPair(int[] values) {
+//         int maxValue = 0;
+//         int cur = 0;
+//         for(int i=0;i<values.length;i++) {
+//             maxValue = Math.max(maxValue,cur + values[i]);
+//             cur = Math.max(cur,values[i]) - 1;
+//         }
+//         return maxValue;
+        
+//     }
+// }
