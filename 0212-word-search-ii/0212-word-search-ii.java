4a5a3bea7fe0c89db6ab1fc88b7696f41a6ee49a
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2025-01-02 15:31:03 +0530

    Time: 117 ms (88.1%), Space: 44.8 MB (84.95%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -0,0 +1,71 @@
+class Solution {
+    public static Node root;
+    public static List<String> result;
+
+    public List<String> findWords(char[][] board, String[] words) {
+        root = new Node();
+        result = new ArrayList<>();
+
+        for (String word : words) {
+            insert(word);
+        }
+
+        int row = board.length;
+        int col = board[0].length;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (root.arr[board[i][j] - 'a'] != null) {
+                    dfs(board, i, j, root);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public void dfs(char[][] board, int i, int j, Node temp) {
+        char currentChar = board[i][j];
+
+        if (currentChar == '#' || temp.arr[currentChar - 'a'] == null)
+            return;
+
+        temp = temp.arr[currentChar - 'a'];
+
+        if (temp.isEnd) {
+            result.add(temp.word);
+            temp.isEnd = false; 
+        }
+
+        board[i][j] = '#';
+
+        if (i > 0) dfs(board, i - 1, j, temp);
+        if (j > 0) dfs(board, i, j - 1, temp);
+        if (i < board.length - 1) dfs(board, i + 1, j, temp);
+        if (j < board[0].length - 1) dfs(board, i, j + 1, temp);
+
+        board[i][j] = currentChar;
+    }
+
+    public static void insert(String word) {
+        Node temp = root;
+        for (char c : word.toCharArray()) {
+            if (temp.arr[c - 'a'] == null) {
+                temp.arr[c - 'a'] = new Node();
+            }
+            temp = temp.arr[c - 'a'];
+        }
+        temp.isEnd = true;
+        temp.word = word; 
+    }
+}
+
+class Node {
+    boolean isEnd;
+    Node[] arr;
+    String word;
+
+    Node() {
+        arr = new Node[26];
+    }
+}
