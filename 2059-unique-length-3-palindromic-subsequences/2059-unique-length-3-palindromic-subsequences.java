90475e8993f50a1149d15130c4d66c18cc5d0b9a
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2025-01-04 15:51:16 +0530

    Time: 200 ms (54.97%), Space: 45.4 MB (60.21%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,38 +1,29 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        int n = s.length();
-        int[] unique = new int[n];
-        Set<Character> set = new HashSet<>();
-        
-        for(int i = 0; i < n; i++) {
-            set.add(s.charAt(i));
-            unique[i] = set.size();
-        }
+        int total = 0;
+        int[] firstIndex = new int[26];
+        int[] lastIndex = new int[26];
+        Arrays.fill(firstIndex, -1);
 
-        Map<Character, int[]> map = new HashMap<>();
-        for(char c: s.toCharArray())
-            map.put(c, new int[]{-1, -1});
-        
-        for(int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            if(map.get(c)[0] == -1)
-                map.get(c)[0] = i;
-            map.get(c)[1] = i;
-        }
-
-        int result = 0;
-
-        for(Map.Entry<Character, int[]> entry : map.entrySet())  {
-            int[] indices = entry.getValue();
-            if(indices[1] - indices[0] >= 2) {
-                Set<Character> between = new HashSet<>();
-                for(int i = indices[0] + 1; i < indices[1]; i++) {
-                    between.add(s.charAt(i));
-                }
-                result += between.size();
+        for(int i = 0; i < s.length(); i++) {
+            int ch = s.charAt(i) - 'a';
+            if(firstIndex[ch] == - 1) {
+                firstIndex[ch] = i;
             }
+            lastIndex[ch] = i;
         }
+        for(int i = 0; i < 26; i++) {
+            if(firstIndex[i] == -1) continue;
 
-        return result;
+            int start = firstIndex[i];
+            int end = lastIndex[i];
+
+            Set<Character> set = new HashSet<>();
+            for(int j = start + 1; j < end; j++) {
+                set.add(s.charAt(j));
+            }
+            total += set.size();
+        }
+        return total;
     }
 }
