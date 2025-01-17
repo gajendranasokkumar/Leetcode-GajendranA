ebb5053d815311595158c90409867fbf1033bb88
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2025-01-17 10:29:59 +0530

    Time: 2 ms (100%), Space: 63.6 MB (6.47%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
@@ -1,29 +1,34 @@
 class Solution {
     public boolean doesValidArrayExist(int[] derived) {
         int n = derived.length;
-        if(derived.length == 1)
-            return derived[0] == 0 ? true : false;
-        boolean one = false, zero = false;
-        if(derived[0] == 1) {
-            int[] nums = new int[n];
-            nums[0] = 0;
-            zero = run(nums, 0, derived);
-            // System.out.println(Arrays.toString(nums));
+        int xor = 0;
+        for(int i=0;i<n;i++) 
+            xor ^= derived[i];
+        
+        return xor == 0;
 
-            nums[0] = 1;
-            one = run(nums, 0, derived);
-            // System.out.println(Arrays.toString(nums));
-        } else {
-            int[] nums = new int[n];
-            nums[0] = 0;
-            zero = run(nums, 0, derived);
-            // System.out.println(Arrays.toString(nums));
 
-            nums[0] = 1;
-            one = run(nums, 0, derived);
-            // System.out.println(Arrays.toString(nums));
-        }
-        return one || zero;
+
+
+        // if/(derived.length == 1)
+        //     return derived[0] == 0 ? true : false;
+        // boolean one = false, zero = false;
+        // if(derived[0] == 1) {
+        //     int[] nums = new int[n];
+        //     nums[0] = 0;
+        //     zero = run(nums, 0, derived);
+
+        //     nums[0] = 1;
+        //     one = run(nums, 0, derived);
+        // } else {
+        //     int[] nums = new int[n];
+        //     nums[0] = 0;
+        //     zero = run(nums, 0, derived);
+
+        //     nums[0] = 1;
+        //     one = run(nums, 0, derived);
+        // }
+        // return one || zero;
     }
     public boolean run(int[] nums, int i, int[] derived) {
         int n = nums.length;
