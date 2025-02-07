128fc0fc018766801f90e97adef02b7e64d4f817
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2025-02-07 18:14:17 +0530

    Time: 12 ms (95.74%), Space: 58.8 MB (86.9%) - LeetHub

--- 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.java
+++ 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.java
@@ -1,25 +1,20 @@
 class Solution {
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++) 
-            adj.add(new ArrayList<>());
-        for(int i=0;i<n;i++) 
-            if(manager[i] != -1)
-                adj.get(manager[i]).add(i);
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{ headID, 0 });
-        int result = 0;
-        while(!q.isEmpty()) {
-            int size = q.size();
-            while(size-- > 0) {
-                int[] cur = q.poll();
-                int from = cur[0];
-                int time = cur[1];
-                result = Math.max(time, result);
-                for(int to : adj.get(from)) 
-                    q.add(new int[]{ to, time + informTime[from] });
-            }
+        int[] memo = new int[n];
+        Arrays.fill(memo, -1);
+        int res= 0;
+        for(int i = 0; i < n; i++) {
+            res = Math.max(res, dfs(i, manager, informTime, memo));
         }
-        return result;
+        return res;
+    }
+    private int dfs(int node, int[] manager,int[] times, int[] memo) {
+        if(node == -1) {
+            return 0;
+        }
+        if(memo[node] != -1) {
+            return memo[node];
+        }
+        return memo[node] = times[node] + dfs(manager[node], manager, times, memo);
     }
 }
