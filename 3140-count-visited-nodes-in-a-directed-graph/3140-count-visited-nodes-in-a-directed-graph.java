391de800807b27a1d7c245ff91b57b52e69eeecd
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2025-02-13 14:03:51 +0530

    Time: 61 ms (67.86%), Space: 85.3 MB (17.86%) - LeetHub

--- 3140-count-visited-nodes-in-a-directed-graph/3140-count-visited-nodes-in-a-directed-graph.java
+++ 3140-count-visited-nodes-in-a-directed-graph/3140-count-visited-nodes-in-a-directed-graph.java
@@ -1,54 +1,60 @@
+import java.util.*;
+
 class Solution {
-    List<Integer> cycleMember;
-    int[] result; 
     public int[] countVisitedNodes(List<Integer> edges) {
         int n = edges.size();
-        int[] indegree = new int[n];
-        result = new int[n];
-        Arrays.fill(result, -1);
-        for(int i : edges) 
-            if(i != -1)
-                indegree[i]++;
         Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<n;i++)
-            if(indegree[i] == 0)
+        int[] dist = new int[n];
+        boolean[] visited = new boolean[n];
+        int[] notCycle = new int[n]; 
+        int[] indegree = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            indegree[edges.get(i)]++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
                 q.add(i);
-        boolean[] topovisited = new boolean[n];
-        while(!q.isEmpty()) {
-            int from = q.poll();
-            topovisited[from] = true;
-            int to = edges.get(from);
-            if (--indegree[to] == 0) 
-                q.add(to);
-        }
-        for(int i=0;i<n;i++) {
-            if(!topovisited[i]) {
-                cycleMember = new ArrayList<>();
-                boolean[] visited = new boolean[n];
-                int count = dfs(edges, visited, i);
-                for(int mem : cycleMember) {
-                    topovisited[mem] = true;
-                    result[mem] = count;
-                }
             }
         }
-        for(int i=0;i<n;i++) {
-            if(result[i] == -1) {
-                boolean[] visited = new boolean[n];
-                result[i] = dfs(edges, visited, i);
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            notCycle[node] = 1;
+            int next = edges.get(node);
+            if (--indegree[next] == 0) {
+                q.add(next);
             }
         }
-        return result;
+
+        for (int i = 0; i < n; i++) {
+            if (notCycle[i] == 0 && !visited[i]) {
+                int[] cnt = new int[1]; 
+                dfs(i, dist, edges, visited, cnt);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (notCycle[i] == 1 && !visited[i]) {
+                dfs2(i, dist, edges, visited);
+            }
+        }
+
+        return dist;
     }
-    public int dfs(List<Integer> edges, boolean[] visited, int from) {
-        if(result[from] != -1)
-            return result[from];
-        if(visited[from])
-            return 0;
-        int count = 0;
-        visited[from] = true;
-        cycleMember.add(from);
-        count += 1 + dfs(edges, visited, edges.get(from));
-        return result[from] = count;
+
+    private void dfs(int node, int[] dist, List<Integer> edges, boolean[] visited, int[] cnt) {
+        if (visited[node]) return;
+        visited[node] = true;
+        cnt[0]++;
+        dfs(edges.get(node), dist, edges, visited, cnt);
+        dist[node] = cnt[0];
+    }
+
+    private int dfs2(int node, int[] dist, List<Integer> edges, boolean[] visited) {
+        if (visited[node]) return dist[node];
+        visited[node] = true;
+        return dist[node] = dfs2(edges.get(node), dist, edges, visited) + 1;
     }
 }
