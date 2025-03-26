f0fa9a0c305067d41b02b3e8b6da72797a217e50
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2025-03-26 20:37:47 +0530

    Time: 37 ms (76.38%), Space: 77.9 MB (22.11%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
@@ -1,28 +1,30 @@
 class Solution {
     public int minOperations(int[][] grid, int x) {
-        List<Integer> values = new ArrayList<>();
-        
-        for (int[] row : grid) {
-            for (int val : row) {
-                values.add(val);
+        int[] arr = new int[grid.length * grid[0].length];
+        int index = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                arr[index++] = grid[i][j];
             }
         }
 
-        Collections.sort(values);
+        Arrays.sort(arr);
+        int median = arr[(arr.length - 1) / 2];
+        int steps = 0;
 
-        for (int val : values) {
-            if (Math.abs(val - values.get(0)) % x != 0) {
+        for (int num : arr) {
+            if (num == median) {
+                continue;
+            }
+
+            if (Math.abs(num - median) % x != 0) {
                 return -1;
             }
+
+            steps += (Math.abs(num - median) / x);
         }
 
-        int median = values.get(values.size() / 2);
-        int operations = 0;
-
-        for (int val : values) {
-            operations += Math.abs(val - median) / x;
-        }
-
-        return operations;
+        return steps;
     }
 }
