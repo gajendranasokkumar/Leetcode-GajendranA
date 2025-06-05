dfce32d4fe132c39132f36ae1636e8edd76a1a5a
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2025-06-05 09:47:28 +0530

    Time: 2 ms (96.73%), Space: 41.9 MB (88.19%) - LeetHub

--- 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.java
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.java
@@ -2,50 +2,53 @@ class Solution {
     public String smallestEquivalentString(String s1, String s2, String baseStr) {
         DisjointSet ds = new DisjointSet(26);
         int n = s1.length();
-        for(int i=0;i<n;i++) {
-            int one = s1.charAt(i) - 'a';
-            int two = s2.charAt(i) - 'a';
-            ds.union(one, two);
-        }
-        String res = "";
-        for(char c : baseStr.toCharArray()) {
-            int num = ds.find(c - 'a');
-            res += (char)(num + 'a');
+
+        for (int i = 0; i < n; i++) {
+            ds.union(s1.charAt(i) - 'a', s2.charAt(i) - 'a');
         }
 
-        return res;
+        StringBuilder res = new StringBuilder();
+        for (char c : baseStr.toCharArray()) {
+            int root = ds.find(c - 'a');
+            res.append((char) (root + 'a'));
+        }
+
+        return res.toString();
+
     }
 }
 
-class DisjointSet{
+class DisjointSet {
     static int[] parent;
     static int[] size;
-    
-    DisjointSet(int n){
-        parent=new int[n];
-        size=new int[n];
+
+    DisjointSet(int n) {
+        parent = new int[n];
+        size = new int[n];
         // Arrays.fill(size,1);
-        for(int i=0;i<n;i++) {
+        for (int i = 0; i < n; i++) {
             parent[i] = i;
             size[i] = i;
         }
     }
-    
-    static int find(int v){
-        if(parent[v]==v) return v;
+
+    static int find(int v) {
+        if (parent[v] == v)
+            return v;
         return parent[v] = find(parent[v]);
     }
-    
-    boolean union(int u,int v){
+
+    boolean union(int u, int v) {
         int ul_u = find(u);
         int ul_v = find(v);
-        if(ul_u == ul_v) return false;
-        
-        if(size[ul_u]>size[ul_v]){
-            parent[ul_u]=ul_v;
+        if (ul_u == ul_v)
+            return false;
+
+        if (size[ul_u] > size[ul_v]) {
+            parent[ul_u] = ul_v;
             // size[ul_v]+=size[ul_u];
-        }else{
-            parent[ul_v]=ul_u;
+        } else {
+            parent[ul_v] = ul_u;
             // size[ul_u]+=size[ul_v];
         }
         return true;
