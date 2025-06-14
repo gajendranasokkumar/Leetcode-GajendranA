74c0e9c533d619f2aac0697d1c2c4447cfd19722
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2025-06-14 21:37:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Symmetric Tree/symmetric-tree.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSymmetric(Node root) {
+        if (root == null) return true; // An empty tree is symmetric
+        return isMirror(root.left, root.right);
+    }
+    
+    private boolean isMirror(Node t1, Node t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+        return (t1.data == t2.data)
+                && isMirror(t1.left, t2.right)
+                && isMirror(t1.right, t2.left);
+    }
+}
