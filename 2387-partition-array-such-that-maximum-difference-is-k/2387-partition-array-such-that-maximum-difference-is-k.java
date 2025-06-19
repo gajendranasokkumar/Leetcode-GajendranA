5f20f448c97573309e3d66692062578695d101f0
Gajendran <130917949+gajendranasokkumar@users.noreply.github.com>
2025-06-19 20:55:31 +0530

    Time: 135 ms (5.32%), Space: 60.7 MB (5.32%) - LeetHub

---
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.java
@@ -0,0 +1,20 @@
+// Approach - 03 - O(nlogn) & O(n)
+class Solution {
+    public int partitionArray(int[] nums, int k) {
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int val : nums) {
+            set.add(val);
+        }
+
+        int grp = 0;
+        int range = -1;
+        for (int val : set) {
+            if (val > range) {
+                range = val + k;
+                grp++;
+            }
+        }
+
+        return grp;
+    }
+}
